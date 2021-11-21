1e9000ee367c2d2fc4ccbd237896d48b82232fc8
LittleRedRidingU <para.bola@qq.com>
2021-11-20 22:15:13 -0500

    road_stuffs

---
+++ Assets/3rdParty/2D Cartoon Environments/Scripts/nextScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nextScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if ( Input.GetKeyDown(KeyCode.N)) SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/3rdParty/2D Cartoon Environments/Scripts/sceneChange1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine. SceneManagement;
+
+public class sceneChange1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void change(int river)
+    { SceneManager.LoadScene(river); }
+}
