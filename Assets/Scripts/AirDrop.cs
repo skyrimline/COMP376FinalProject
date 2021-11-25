9b36a654908843f9fc9c73aadc298061d9e51f54
Ray-Leonard <rayleonard726@gmail.com>
2021-11-24 22:50:48 -0500

    1

--- Assets/Scripts/AirDrop.cs
+++ Assets/Scripts/AirDrop.cs
@@ -5,33 +5,40 @@ using UnityEngine.EventSystems;
 
 public class AirDrop : MonoBehaviour, IPointerDownHandler
 {
-    private int food_supply;
-    private int money_supply;
-    private int vaccineA_supply;
-    private int vaccineB_supply;
-    private int vaccineC_supply;
+
     private int supply_label1;
     private int supply_label2;
+
+    private int[] supplys;
 
     private GameLogic gameLogicReference;
 
     private bool isClickable = false;
 
+    [SerializeField] private GameObject FloatingResourcePrefab;
+    [SerializeField] private Sprite[] resourceSprs;
+
     // Start is called before the first frame update
     void Start()
     {
+        int food_supply;
+        int money_supply;
+        int vaccineA_supply;
+        int vaccineB_supply;
+        int vaccineC_supply;
         //initialized the number of resources
         food_supply = Random.Range(5, 10);
         money_supply = Random.Range(50, 100);
         vaccineA_supply = Random.Range(3, 7);
         vaccineB_supply = Random.Range(5, 11);
         vaccineC_supply = Random.Range(1, 3);
+        int[] supplys_ = { food_supply, money_supply, vaccineA_supply, vaccineB_supply, vaccineC_supply};
+        supplys = supplys_;
 
         //initialized the labels of supply
          supply_label1 = Random.Range(0, 5);
          supply_label2 = Random.Range(0, 5);
 
-        //
         gameLogicReference = GameObject.FindGameObjectsWithTag("GameLogic")[0].GetComponent<GameLogic>();
     }
 
@@ -60,23 +67,23 @@ public class AirDrop : MonoBehaviour, IPointerDownHandler
         {
             case 0:
                 // give food
-                gameLogicReference.foodNum += food_supply;
+                gameLogicReference.foodNum += supplys[supply_label1];
                 break;
             case 1:
                 // give money
-                gameLogicReference.money += money_supply;
+                gameLogicReference.money += supplys[supply_label1];
                 break;
             case 2:
                 // give vaccineA
-                gameLogicReference.vaccineA_num += vaccineA_supply;
+                gameLogicReference.vaccineA_num += supplys[supply_label1];
                 break;
             case 3:
                 // give vaccineB
-                gameLogicReference.vaccineB_num += vaccineB_supply;
+                gameLogicReference.vaccineB_num += supplys[supply_label1];
                 break;
             case 4:
                 // give vaccineC
-                gameLogicReference.vaccineC_num += vaccineC_supply;
+                gameLogicReference.vaccineC_num += supplys[supply_label1];
                 break;
 
         }
@@ -85,27 +92,55 @@ public class AirDrop : MonoBehaviour, IPointerDownHandler
         {
             case 0:
                 // give food
-                gameLogicReference.foodNum += food_supply;
+                gameLogicReference.foodNum += supplys[supply_label2];
                 break;
             case 1:
                 // give money
-                gameLogicReference.money += money_supply;
+                gameLogicReference.money += supplys[supply_label2];
                 break;
             case 2:
                 // give vaccineA
-                gameLogicReference.vaccineA_num += vaccineA_supply;
+                gameLogicReference.vaccineA_num += supplys[supply_label2];
                 break;
             case 3:
                 // give vaccineB
-                gameLogicReference.vaccineB_num += vaccineB_supply;
+                gameLogicReference.vaccineB_num += supplys[supply_label2];
                 break;
             case 4:
                 // give vaccineC
-                gameLogicReference.vaccineC_num += vaccineC_supply;
+                gameLogicReference.vaccineC_num += supplys[supply_label2];
                 break;
 
         }
-
+        SpawnFloatingResourceInfo();
         Destroy(gameObject);
+    }
+
+    private void SpawnFloatingResourceInfo()
+    {
+        // need to spawn 2 of these
+        Vector3 pos1 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pos1.z = 0;
+        pos1.y += 1f;
+        pos1.x -= 1f;
+        GameObject g1 = Instantiate(FloatingResourcePrefab, pos1, Quaternion.identity);
+        var f1 = g1.GetComponentInChildren<Floating_Info_Control>();
+        if (f1 != null)
+        {
+            f1.SetText("+" + supplys[supply_label1].ToString("D3"));
+            f1.SetImage(resourceSprs[supply_label1]);
+        }
+
+        Vector3 pos2 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pos2.z = 0;
+        pos2.y += 1f;
+        pos2.x += 3f;
+        GameObject g2 = Instantiate(FloatingResourcePrefab, pos2, Quaternion.identity);
+        var f2 = g2.GetComponentInChildren<Floating_Info_Control>();
+        if (f2 != null)
+        {
+            f2.SetText("+" + supplys[supply_label2].ToString("D3"));
+            f2.SetImage(resourceSprs[supply_label2]);
+        }
     }
 }
